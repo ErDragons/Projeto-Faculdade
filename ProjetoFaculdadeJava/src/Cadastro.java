9b542017639532045c36cea33a9b8f451ab19ea8
ErDragons <64704184+ErDragons@users.noreply.github.com>
2020-05-02 16:47:15 -0300

    PROJETOFACULDADE - Parte em Java

---
+++ ProjetoFaculdadeJava/src/Cadastro.java
@@ -0,0 +1,101 @@
+public class Cadastro {
+   private String id;
+   private String nome;
+   private String sobrenome;
+   private String cpf;
+   private String dataNascimento;
+   private String email;
+   private String telefone;
+   private String endereco;
+   private String cargo;
+   private String salario;
+   private String senha;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getSalario() {
+        return salario;
+    }
+
+    public void setSalario(String salario) {
+        this.salario = salario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ ProjetoFaculdadeJava/src/ProjetoFaculdade.java
@@ -0,0 +1,7 @@
+public class ProjetoFaculdade {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
